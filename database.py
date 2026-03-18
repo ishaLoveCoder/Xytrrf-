aff8716259dbe25be0df5d9d61a6c6e7fd4a7a45
ishaLoveCoder <isha1470@outlook.com>
2026-03-18 11:55:58 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,9 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["movie_file_db"]
+
+files = db["files"]
